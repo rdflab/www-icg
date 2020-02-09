a6c8a50d0ef33480850671f6dda77f43b856c19e
Antony B Holmes <antony.b.holmes@gmail.com>
2020-02-09 02:10:40 -0500

    Bug fixes and updates.

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,19 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import headerStyles from "./header.module.scss"
+import Container from "react-bootstrap/Container"
 
 const Header = ({ siteTitle }) => (
   <header className={headerStyles.header}>
-    <div style={{padding: `20px`}}>INSTITUTE FOR <strong>CANCER GENETICS</strong></div>
-
-    <div style={{backgroundColor: `rgba(0, 0, 0, 0.2)`, padding:`10px`}}>
+    <Container>
+    <div style={{padding: `10px`, color: `rgba(255, 255, 255, 0.8)`}}>INSTITUTE FOR CANCER GENETICS</div>
+    </Container>
+    <div style={{backgroundColor: `rgba(0, 0, 0, 0)`}}>
+    <Container>
       <Link to={'/research-areas/labs/'}>Reasearch Labs</Link>
+      </Container>
     </div>
+    
   </header>
 )
 
