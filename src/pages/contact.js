510713f9dd5dca644563a54533af5bb0d84d7f4f
Antony Holmes <antony.b.holmes@gmail.com>
2020-03-04 20:26:18 -0500

    Bug fixes and updates.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -25,7 +25,7 @@ const Contact = () => (
             src="https://www.openstreetmap.org/export/embed.html?bbox=-73.94093066453935%2C40.839045405350824%2C-73.93842011690141%2C40.840475992930344&amp;layer=mapnik"
           ></iframe> */}
         <iframe
-          className="block mx-auto"
+          className="block mx-auto sm:hidden"
           align="center"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3018.4630742859304!2d-73.94173524903421!3d40.839755837625155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2f69cf9446ff1%3A0x523dcfdc4ca47584!2s1130%20St%20Nicholas%20Ave%2C%20New%20York%2C%20NY%2010032!5e0!3m2!1sen!2sus!4v1583356250505!5m2!1sen!2sus"
           width="400"
