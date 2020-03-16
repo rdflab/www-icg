e14d8afae54e7bccc5acfe20e7226ac13c9a9544
Antony Holmes <antony.b.holmes@gmail.com>
2020-03-16 19:35:25 -0400

    Bug fixes and updates.

--- indexsite.py
+++ indexsite.py
@@ -8,6 +8,14 @@ Created on Mon Mar 16 17:34:05 2020
 import os
 from frontmatter import Frontmatter
 import json
+import collections
+
+PEOPLE_TYPES = [
+  'Faculty',
+  'Research Scientist',
+  'Graduate Student',
+  'Staff',
+]
 
 def create_suffix_tree(root, text, item):
     words = text.lower().split(' ')
@@ -52,7 +60,7 @@ if link not in linkNameMap:
 
 siteData['sections'].append('Labs')
 dir = 'src/data/groups'
-for file in sorted(os.listdir('src/data/groups')):
+for file in sorted(os.listdir(dir)):
     print(file)
     post = Frontmatter.read_file(os.path.join(dir, file))
     print(post)
@@ -67,6 +75,50 @@ for file in sorted(os.listdir('src/data/groups')):
     siteData['links'].append([name , si, linkNameMap[link], to])
 si += 1
 
+
+#
+# People
+#
+
+people = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(str)))
+
+dir = 'src/data/people'
+for file in sorted(os.listdir(dir)):
+    post = Frontmatter.read_file(os.path.join(dir, file))
+    
+    id = post['attributes']['id']
+    first = post['attributes']['firstName']
+    last = post['attributes']['lastName']
+    t = post['attributes']['type']
+    
+    print(post, dir, file)
+    
+    people[t][last][first] = id
+
+siteData['sections'].append('People')
+
+for t in PEOPLE_TYPES:
+    link = t
+    
+    #siteData['sections'].append(t)
+    
+    if link not in linkNameMap:
+        linkNameMap[link] = len(siteData['linkNames'])
+        siteData['linkNames'].append(link)
+    
+    for last in sorted(people[t]):
+        for first in sorted(people[t][last]):
+            id = people[t][last][first]
+            
+            name = '{} {}'.format(first, last)
+            
+            to = '/research-areas/faculty-and-staff/{}'.format(id)
+    
+            create_suffix_tree(siteData['tree'], name, len(siteData['links']))
+    
+            siteData['links'].append([name, si, linkNameMap[link], to])
+
+si += 1
 
 #
 # Publications
@@ -102,5 +154,5 @@ si += 1
     
     
     
-json.dump(siteData, open('static/site.index.json', 'w')) #, indent=2)
+json.dump(siteData, open('static/site.index.json', 'w'), separators=(',', ':')) #, indent=2)
        
