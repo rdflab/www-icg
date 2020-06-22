2478974af1df1a9d55c6df3d0a6b4f8c8a2010e4
Antony B Holmes <antony.b.holmes@gmail.com>
2020-06-21 22:42:06 -0400

    Bug fixes and updates.

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,9 @@
 import React from "react"
 
-const Card = ({ children, className }) => (
+const Card = ({ children, onMouseEnter, onMouseLeave, className }) => (
   <div
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
     className={`relative trans-ani rounded-lg overflow-hidden w-full bg-white shadow hover:shadow-md ${className}`}
   >
     {children}
@@ -10,6 +12,8 @@ const Card = ({ children, className }) => (
 
 Card.defaultProps = {
   className: "",
+  onMouseEnter: null,
+  onMouseLeave: null,
 }
 
 export default Card
